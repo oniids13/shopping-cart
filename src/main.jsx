5000761244bea044ae04989a9af00428ccc1c080
oniids <jdino.abaya@gmail.com>
2025-01-24 16:56:35 +0800

    Add view and component directories. Create pages for the view directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Home from './views/Home'
+import ErrorPage from './views/ErrorPage'
+
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+    path='/'
+    element={<Home/>}
+    errorElement={<ErrorPage/>}
+    />
+  )
+)
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/views/ErrorPage.jsx
@@ -0,0 +1,13 @@
+
+
+const ErrorPage = () => {
+    return(
+        <>
+            <h1>Oh no! The Page your looking for doesn't exist</h1>
+            <button>Go Back!</button>
+        </>
+    )
+}
+
+
+export default ErrorPage
